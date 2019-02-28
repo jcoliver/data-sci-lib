69dbaf61f3fb160480a483b60a9e867b31c6dc02
jcoliver <jcoliver@email.arizona.edu>
2019-02-28 16:33:44 -0700

    File cleanup and add marketing

--- create-cover-docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-INFILE="data-sci-lib-cover.md"
-OUTPDF="Oliver-et-al-Cover.pdf"
-OUTDOCX="Oliver-et-al-Cover.docx"
-
-pandoc $INFILE -f markdown -o $OUTDOCX --latex-engine=xelatex --reference-docx data-sci-lib-pandoc-ref.docx
-pandoc $INFILE -f markdown -o $OUTPDF --latex-engine=xelatex -V geometry:margin=0.75in -V urlcolor=blue
--- create-ms-docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-INFILE="data-sci-lib-ms.md"
-OUTPDF="Oliver-et-al-MS.pdf"
-OUTDOCX="Oliver-et-al-MS.docx"
-
-pandoc $INFILE -f markdown -o $OUTDOCX --latex-engine=xelatex --reference-docx data-sci-lib-pandoc-ref.docx
-pandoc $INFILE -f markdown -o $OUTPDF --latex-engine=xelatex -V geometry:margin=0.75in -V urlcolor=blue
